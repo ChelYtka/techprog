d08724a143b3c77e6ef739b1c97ca3dd23af1944
Egor <ead38@tpu.ru>
2025-12-02 19:31:24 +0700

    доавлен функционал в трениовку

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -57,7 +57,7 @@ void MainWindow::onBtnTrainingClicked()
     }
 
     showOnlyMainMenu();
-    ui->groupBoxTraining->setVisible(true);
+    ui->groupBoxTraining->setVisible(false);
 
     // Очищаем и показываем начало тренировки
     ui->tableDictionary->clear();
